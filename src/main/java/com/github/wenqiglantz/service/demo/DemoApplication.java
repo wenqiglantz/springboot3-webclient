743a3c51703f11ad1012559d012523d4d4d314f2
Wenqi Glantz <wenqiglantz@gmail.com>
2022-10-25 23:13:56 -0400

    Update DemoApplication.java
    added pkg org.springframework.cloud.function.context scan in main class

--- src/main/java/com/github/wenqiglantz/service/demo/DemoApplication.java
+++ src/main/java/com/github/wenqiglantz/service/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"org.springframework.web.reactive.function.client"})
+@ComponentScan({"org.springframework.web.reactive.function.client", "org.springframework.cloud.function.context"})
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
