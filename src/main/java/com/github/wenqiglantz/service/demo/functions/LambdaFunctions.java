a760716f511ad0977716032706e3c387ca77b1b0
wglantz <wglantz@arisglobal.com>
2022-11-02 15:25:42 -0400

    revised function

--- src/main/java/com/github/wenqiglantz/service/demo/functions/LambdaFunctions.java
+++ src/main/java/com/github/wenqiglantz/service/demo/functions/LambdaFunctions.java
@@ -1,39 +1,22 @@
 package com.github.wenqiglantz.service.demo.functions;
 
-import com.github.wenqiglantz.service.demo.data.DemoRequest;
-import com.github.wenqiglantz.service.demo.data.DemoResponse;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
-import java.net.URI;
-import java.util.function.Function;
+import java.util.function.Consumer;
 
 @Configuration
 @Slf4j
 @RequiredArgsConstructor
 public class LambdaFunctions {
 
-    private static final String BASE_URL = "http://########.com/api";
-
-    private final WebClient webClient;
-
     @Bean
-    public Function<DemoRequest, Mono<DemoResponse>> records() {
+    public Consumer<APIGatewayProxyRequestEvent> demo() {
         return (request) -> {
-            return webClient.post()
-                    .uri(URI.create(BASE_URL))
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaType.APPLICATION_JSON)
-                    .body(BodyInserters.fromValue(request))
-                    .exchange()
-                    .block()
-                    .bodyToMono(DemoResponse.class);
+            log.debug(">>> request " + request.getBody());
         };
     }
 }
